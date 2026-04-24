2810677e9ed88f331e6b616ee6338e20aba0b8c1
ana-clara-gomes-saraiva <anaclaragomessaraiva1@gmail.com>
2026-04-24 17:44:21 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,16 +2,6 @@ import streamlit as st
 
 st.title("Calculadora de Notas do IF")
 
-n1 = st.text_input("Introduza a Nota 1")
-n2 = st.text_input("Introduza a Nota 2")
-
-if st.button("Calcular Média"):
-  media = (float(n1) + float(n2)) / 2
-  st.write("A tua média é:", media)
-import streamlit as st
-
-st.title("📊 Calculadora de Notas do IF")
-
 st.write("Insira notas (podem ser decimais, ex: 7.5)")
 
 # Inputs aceitando decimais
